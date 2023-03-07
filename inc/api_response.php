c1517202f5aafb34542e6caad42ad2ee0c96c5a0
Otto Micheletti <michelettiotto@gmail.com>
2023-03-07 14:29:28 -0300

    feat: :sparkles: Criação da classe responsável por gerar um Response na API em formato JSON.

---
+++ inc/api_response.php
@@ -0,0 +1,65 @@
+<?php
+class Response
+{
+  private $data;
+  private $available_methods = ['GET', 'POST', 'PUT', 'DELETE'];
+
+  public function __construct()
+  {
+    $this->data = [];
+  }
+
+  public function check_method($method)
+  {
+    return in_array($method, $this->available_methods);
+  }
+
+  //---------------------
+  // GETTERS & SETTERS
+  //---------------------
+  public function set_method($method)
+  {
+    $this->data['method'] = $method;
+  }
+
+  public function get_method()
+  {
+    return $this->data['method'];
+  }
+
+  public function set_endpoint($endpoint)
+  {
+    $this->data['endpoint'] = $endpoint;
+  }
+
+  public function get_endpoint()
+  {
+    return $this->data['endpoint'];
+  }
+
+  //---------------------
+  // Métodos genéricos
+  //---------------------
+  public function api_request_error($message = '')
+  {
+    $data_error = [
+      'status' => 'ERROR',
+      'message' => $message,
+    ];
+    $this->data['data'] = $data_error;
+    $this->send_response();
+  }
+
+  public function add_to_data($key, $value)
+  {
+    $this->data[$key] = $value;
+  }
+
+  public function send_response()
+  {
+    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($this->data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
+    die();
+  }
+}
