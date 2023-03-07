38f9379568a25059d1b3a5cbeaa838acb5dd6f32
Otto Micheletti <michelettiotto@gmail.com>
2023-03-07 15:34:28 -0300

    feat: :sparkles: Carrega o arquivo 'database.php' no index da API.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // https://youtu.be/gsPp71nRMQk
 
 date_default_timezone_set("America/Sao_Paulo");
-$path = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2); // conteudo da barra após api-php/[0]/[1] onde [0] é o endpoint e o [1] argumento.
+$path = array_slice(explode('/', $_SERVER['REQUEST_URI']), 2); // conteudo da barra após api-php/[0]/[1] onde [0] é o endpoint e o [1] argumento
 $req_method = $_SERVER['REQUEST_METHOD']; // qual método HTTP a página foi chamada
 $req_body = json_decode(file_get_contents('php://input')); // objeto do body da requisição HTTP, acessar as chaves do body com: $req_body->exemplo
 
@@ -12,6 +12,7 @@ $req_body = json_decode(file_get_contents('php://input')); // objeto do body da 
 require_once(dirname(__FILE__) . '/inc/config.php');
 require_once(dirname(__FILE__) . '/inc/api_response.php');
 require_once(dirname(__FILE__) . '/inc/api_request.php');
+require_once(dirname(__FILE__) . '/inc/database.php');
 
 // instanciação da classe Response
 $api = new Response();
@@ -29,30 +30,30 @@ if (!$api->check_method($req_method)) {
 // prepara a requisição conforme o verbo HTTP
 switch ($req_method) {
   case 'GET':
-    //echo "entrou no GET";
+    //echo "entrou no GET"
     $api_req = new Request($api->get_endpoint());
     break;
   case 'POST':
-    //echo "entrou no POST";
+    //echo "entrou no POST"
     $api_req = new Request($api->get_endpoint(), null, $req_body);
     break;
   case 'PUT':
-    //echo "entrou no PUT";
+    //echo "entrou no PUT"
     if ($path[1]) {
-      //echo "entrou no if";
+      //echo "entrou no if"
       $api_req = new Request($api->get_endpoint(), $path[1], $req_body);
     } else {
-      //echo "entrou no else";
+      //echo "entrou no else"
       $api->api_request_error("'PUT' and 'DELETE' HTTP methods require a parameter after the endpoint.");
     }
     break;
   case 'DELETE':
-    //echo "entrou no DELETE";
+    //echo "entrou no DELETE"
     if ($path[1]) {
-      //echo "entrou no if";
+      //echo "entrou no if"
       $api_req = new Request($api->get_endpoint(), $path[1]);
     } else {
-      //echo "entrou no else";
+      //echo "entrou no else"
       $api->api_request_error("'PUT' and 'DELETE' HTTP methods require a parameter after the endpoint.");
     }
     break;
