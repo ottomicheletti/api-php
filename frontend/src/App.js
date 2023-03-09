19c9794873b3e40f9d4246085a3cebfea594702b
Otto Micheletti <michelettiotto@gmail.com>
2023-03-09 19:12:40 -0300

    feat: :fire: Remoção do antigo frontend e inicialização de um novo projeto com create-react-app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Venda from './Pages/Venda';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" >
-        <Route index element={<Navigate to='venda' />} />
-        <Route path="venda" element={<Venda />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
