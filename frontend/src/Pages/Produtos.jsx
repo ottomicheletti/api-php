3d492de10a781108daf0106df4aa61484824afea
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 08:21:31 -0300

    feat: :sparkles: Criação do componente Produtos para a rota '/produtos'.

---
+++ frontend/src/Pages/Produtos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+function Produtos() {
+  return (
+    <Layout>
+      <div>Estou na página de produtos</div>
+    </Layout>
+  );
+}
+
+export default Produtos;
