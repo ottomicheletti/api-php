c8c40e5c02071b97f0ea56f4a75ee209cc5fcfbe
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 15:57:29 -0300

    feat: :recycle: Refatoração da UI e nova lógica para o componente Venda.

--- frontend/src/Pages/Venda.jsx
+++ frontend/src/Pages/Venda.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../Components/Layout';
 import request from '../Helpers/request';
 import { useStore } from '../Store/Store';
@@ -7,9 +7,36 @@ import { v4 as uuidv4 } from 'uuid';
 import './Venda.css';
 
 function Venda() {
-  const { produtos, setProdutos, venda, setVenda, setTotal } = useStore((state) => state);
+  const {
+    produtos,
+    // carrinho,
+    venda,
+    setProdutos,
+    setTotal,
+    setCarrinho,
+    setMessage,
+    clearVenda,
+    handleChange,
+  } = useStore((state) => state);
 
-  // const [relatorio, setRelatorio] = useState([]);
+  const handleClick = (name) => {
+    if (venda.total === null || venda.total > 0) {
+      switch (name) {
+        case 'incluir':
+          setCarrinho();
+          setMessage({ text: 'Produto incluído com sucesso!', type: 'success' });
+          clearVenda();
+          break;
+        case 'concluir':
+          // implementar esse case
+          break;
+        default:
+          break;
+      }
+    } else {
+      setMessage({ text: 'Verifique seus inputs.', type: 'error' });
+    }
+  };
 
   useEffect(() => {
     request('produtos', 'GET').then((response) => setProdutos(response));
@@ -22,13 +49,15 @@ function Venda() {
   return (
     <Layout>
       <div id='venda'>
-        <div className='stack'>
+        <div className='display-block'>
           <div className='input-default'>
             <div>Produto</div>
-            <select name='produto' onChange={(e) => setVenda(e)} defaultValue='DEFAULT'>
-              <option value='DEFAULT' disabled hidden>
-                Escolha um produto
-              </option>
+            <select
+              name='produto'
+              id='produto'
+              onChange={handleChange}
+              value={venda.produto}
+            >
               {produtos.map((produto) => (
                 <option value={produto.codigo} key={uuidv4()}>
                   {`${('00' + produto.codigo).slice(-2)} - ${produto.nome}`}
@@ -36,16 +65,33 @@ function Venda() {
               ))}
             </select>
           </div>
-          <div className='input-default'>
-            <div>Quantidade</div>
-            <input
-              type='number'
-              name='quantidade'
-              min='0'
-              value={venda.quantidade}
-              onChange={(e) => setVenda(e)}
-            />
+          <div className='display-column'>
+            <div className='input-default'>
+              <div>Quantidade</div>
+              <input
+                type='number'
+                name='quantidade'
+                min='0'
+                value={venda.quantidade}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='input-default'>
+              <div>Valor (un)</div>
+              <div>{`R$${venda.produto ? produtos[venda.produto - 1]?.valor : 0}`}</div>
+            </div>
+            <div className='input-default'>
+              <div>Total</div>
+              <div>{`R$${venda?.total ? venda.total : 0}`}</div>
+            </div>
           </div>
+          <button
+            className='button-default'
+            name='incluir'
+            onClick={({ target: { name } }) => handleClick(name)}
+          >
+            Incluir
+          </button>
         </div>
         <div id='divider'></div>
         <div className='stack'></div>
