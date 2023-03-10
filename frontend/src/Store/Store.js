11e5e3c7d7395c325c06820992c521ceb1e4ad8b
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 15:58:15 -0300

    feat: :sparkles: Inclusão de novos estado globais na store do Zustand.

--- frontend/src/Store/Store.js
+++ frontend/src/Store/Store.js
@@ -4,30 +4,62 @@ import { devtools } from 'zustand/middleware';
 export const useStore = create(devtools(
   (set) => ({
     produtos: [],
+    carrinho: [],
     venda: {
-      produto: 0,
+      produto: 1,
       quantidade: 0,
       total: 0,
+    },
+    message: {
+      text: '',
+      type: '',
     },
 
     setProdutos: (data) => set(() => ({
       produtos: data,
     }), false, 'setProdutos'),
 
-    setVenda: ({ target: { name, value } }) => set((state) => ({
-      ...state,
-      venda: {
-        ...state.venda,
-        [name]: parseInt(value)
-      }
-    }), false, `setVenda-${name}`),
-
     setTotal: () => set((state) => ({
-      ...state,
       venda: {
         ...state.venda,
         total: state.produtos[state.venda.produto - 1]?.valor * state.venda.quantidade,
       }
     }), false, 'setTotal'),
+
+    setCarrinho: () => set((state) => ({
+      carrinho: [...state.carrinho, state.venda]
+    }), false, 'setCarrinho'),
+
+    setMessage: ({text, type}) => set(() => ({
+      message: {
+        text,
+        type,
+      }
+    }), false, 'setMessage'),
+
+    clearMessage: () => set((state) => ({
+      ...state,
+      message: {
+        text: '',
+        type: '',
+      }
+    }), false, 'clearMessage'),
+
+    clearVenda: () => set((state) => ({
+      ...state,
+      venda: {
+        produto: 1,
+        quantidade: 0,
+        total: 0,
+      }
+    }), false, 'clearVenda'),
+
+    handleChange: ({ target: { name, value } }) => set((state) => ({
+      ...state,
+      venda: {
+        ...state.venda,
+        [name]: parseInt(value)
+      }
+    }), false, `handleChange-${name}`),
 }),
 ));
