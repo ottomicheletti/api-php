f4a1f0812478639073db476801ad0ad58f5cc032
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 12:16:15 -0300

    feat: :sparkles: Criação e estilização do componente Venda.

--- frontend/src/Pages/Venda.jsx
+++ frontend/src/Pages/Venda.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import Layout from '../Components/Layout';
+import request from '../Helpers/request';
+import { useStore } from '../Store/Store';
+import { v4 as uuidv4 } from 'uuid';
+import './Venda.css';
 
 function Venda() {
+  const { produtos, setProdutos, venda, setVenda, setTotal } = useStore((state) => state);
+
+  // const [relatorio, setRelatorio] = useState([]);
+
+  useEffect(() => {
+    request('produtos', 'GET').then((response) => setProdutos(response));
+  }, []);
+
+  useEffect(() => {
+    setTotal();
+  }, [venda.produto, venda.quantidade]);
+
   return (
     <Layout>
-      <div>Estou na página de vendas</div>
+      <div id='venda'>
+        <div className='stack'>
+          <div className='input-default'>
+            <div>Produto</div>
+            <select name='produto' onChange={(e) => setVenda(e)} defaultValue='DEFAULT'>
+              <option value='DEFAULT' disabled hidden>
+                Escolha um produto
+              </option>
+              {produtos.map((produto) => (
+                <option value={produto.codigo} key={uuidv4()}>
+                  {`${('00' + produto.codigo).slice(-2)} - ${produto.nome}`}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='input-default'>
+            <div>Quantidade</div>
+            <input
+              type='number'
+              name='quantidade'
+              min='0'
+              value={venda.quantidade}
+              onChange={(e) => setVenda(e)}
+            />
+          </div>
+        </div>
+        <div id='divider'></div>
+        <div className='stack'></div>
+      </div>
     </Layout>
   );
 }
