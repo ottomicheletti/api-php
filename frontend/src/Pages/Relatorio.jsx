13f14d6593e457a6dfbc8addae75ad28b62aa82e
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 08:22:17 -0300

    feat: :sparkles: Criação do componente Relatorio para a rota '/relatorio'.

---
+++ frontend/src/Pages/Relatorio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+function Relatorio() {
+  return (
+    <Layout>
+      <div>Estou na página de relatorio</div>
+    </Layout>
+  );
+}
+
+export default Relatorio;
