04cc79e2e56853dc708c14a44f105dcbc112b2f7
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 15:57:49 -0300

    feat: :sparkles: Inclusão do componente Message no Layout.

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import './Layout.css';
+import Message from './Message';
 
 function Layout({ children }) {
   return (
     <div id='layout'>
       <Header />
       {children}
+      <Message />
     </div>
   );
 }
