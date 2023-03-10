6887fbb000f66b15c994e5eeef54139331a716ea
Otto Micheletti <michelettiotto@gmail.com>
2023-03-10 08:21:09 -0300

    feat: :sparkles: Criação do componente Venda para a rota '/venda'.

---
+++ frontend/src/Pages/Venda.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+function Venda() {
+  return (
+    <Layout>
+      <div>Estou na página de vendas</div>
+    </Layout>
+  );
+}
+
+export default Venda;
