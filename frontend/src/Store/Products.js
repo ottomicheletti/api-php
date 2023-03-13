d777d1eb2dde255e852ad50d0aab9b636674d954
Otto Micheletti <michelettiotto@gmail.com>
2023-03-13 12:14:18 -0300

    feat: :sparkles: Criação de um estado global para a página '/produtos'.

---
+++ frontend/src/Store/Products.js
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import request from '../Helpers/request';
+import produce from 'immer';
+
+export const productsStore = create(devtools(
+  (set, get) => ({
+    products: [],
+    product: [],
+    edit: [],
+
+    fetchProducts: async () => {
+      const response = await request('produtos', 'GET');
+      const onEdit = response.map((obj) => obj = false);
+      set({ edit: onEdit });
+      set({ products: await response }, false, 'fetchProducts');
+    },
+
+    setOnEdit: (idx, bool) => {
+      const newOnEdit = get().edit.map((b, index) => index === idx ? b = bool : b = !bool  );
+      set({ edit: newOnEdit });
+    },
+
+    confirmOnEdit: (idx, bool) => set((state) => {
+      state.edit[idx] = bool;
+    }, false, 'updateCart'),
+
+
+}), {name: 'Loja do Mirante - productsStore'}));
