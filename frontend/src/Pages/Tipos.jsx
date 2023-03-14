722c5fa2248624da05a247450e605b54a60073b0
Otto Micheletti <michelettiotto@gmail.com>
2023-03-14 11:06:01 -0300

    feat: :sparkles: Criação do componente Tipos para a rota '/tipos'.

--- frontend/src/Pages/Tipos.jsx
+++ frontend/src/Pages/Tipos.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../Components/Layout';
+import TypesTable from './../Components/TypesTable';
 
 function Tipos() {
   return (
     <Layout>
-      <div>Estou na página tipos de produto</div>
+      <TypesTable />
     </Layout>
   );
 }
