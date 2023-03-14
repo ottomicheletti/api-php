97bf33914fbc303dd528ddb7dbeb90b835a48aea
Otto Micheletti <michelettiotto@gmail.com>
2023-03-14 10:04:55 -0300

    feat: :sparkles: Nova lógica para inserir produtos novos no banco de dados.

--- frontend/src/Components/ProductsTable.jsx
+++ frontend/src/Components/ProductsTable.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { messageStore } from '../Store/Message';
 import { productsStore } from '../Store/Products';
-import { Trash, Pencil, CheckFat } from '@phosphor-icons/react';
+import {
+  Trash,
+  Pencil,
+  CheckFat,
+  PlusCircle,
+  XCircle,
+  CheckCircle,
+} from '@phosphor-icons/react';
 import request from '../Helpers/request';
 import './Tables.css';
 
@@ -20,6 +27,13 @@ function ProductsTable() {
     onEditProduct,
   } = productsStore((state) => state);
   const { setMessage } = messageStore((state) => state);
+
+  const [enabled, setEnabled] = useState(false);
+  const [newProduct, setNewProduct] = useState({
+    nome: '',
+    valor: 0,
+    tipo: 1,
+  });
 
   const editProduct = (index) => {
     setProduct(index);
@@ -43,129 +57,212 @@ function ProductsTable() {
     setMessage({ text: 'Produto excluído!', type: 'ok' });
   };
 
+  const insertNewProduct = async () => {
+    if (newProduct.nome !== '' && newProduct.valor > 0) {
+      await request('produtos', 'POST', {
+        nome: newProduct.nome,
+        valor: newProduct.valor,
+        tipo: newProduct.tipo,
+      });
+      setMessage({ text: 'Produto adicionado!', type: 'ok' });
+      setEnabled(false);
+      setNewProduct({
+        nome: '',
+        valor: 0,
+        tipo: 1,
+      });
+      fetchProducts();
+    } else {
+      setMessage({ text: 'Verifique seus inputs.', type: 'fail' });
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
-    <table>
-      <thead>
-        <tr className='table-divider'>
-          <th>Produto</th>
-          <th>Valor (un)</th>
-          {/* <th>Tipo</th> */}
-          <th>Categoria</th>
-          <th>% de Imposto</th>
-        </tr>
-        <tr>
-          <th colSpan={4}>
-            <hr />
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {products &&
-          products.map(
-            ({ codigo, nome, valor, tipo, categoria, percentual_imposto }, index) => (
-              <tr key={codigo} className='table-data'>
-                <th>
-                  {`${('00' + codigo).slice(-2)} - `}
-                  {edit[index] ? (
-                    <input
-                      type='text'
-                      name='nome'
-                      value={product.nome}
-                      onChange={onEditProduct}
-                    />
-                  ) : (
-                    <span>{nome}</span>
-                  )}
-                </th>
-                <th>
-                  {edit[index] ? (
-                    <input
-                      type='number'
-                      name='valor'
-                      value={parseFloat(product.valor)}
-                      onChange={onEditProduct}
-                    />
-                  ) : (
-                    <span>
-                      {valor.toLocaleString('pt-BR', {
-                        style: 'currency',
-                        currency: 'BRL',
-                      })}
-                    </span>
-                  )}
-                </th>
-                {/* <th>{tipo}</th> */}
-                <th>
-                  {edit[index] ? (
-                    <select name='tipo' onChange={onEditProduct} value={product.tipo}>
-                      {types.map((type) => (
-                        <option value={type.codigo} key={uuidv4()}>
-                          {type.nome}
-                        </option>
-                      ))}
-                    </select>
-                  ) : (
-                    <span>{categoria}</span>
-                  )}
-                </th>
-                <th>
-                  <div className='row-total'>
+    <div id='table'>
+      <table>
+        <thead>
+          <tr className='table-divider'>
+            <th>Produto</th>
+            <th>Valor (un)</th>
+            <th>Categoria</th>
+            <th>% de Imposto</th>
+          </tr>
+          <tr>
+            <th colSpan={4}>
+              <hr />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {products &&
+            products.map(
+              ({ codigo, nome, valor, tipo, categoria, percentual_imposto }, index) => (
+                <tr key={codigo} className='table-data'>
+                  <th>
+                    {`${('00' + codigo).slice(-2)} - `}
                     {edit[index] ? (
-                      <span>
-                        {(
-                          types.find((type) => type.codigo === product.tipo)
-                            ?.percentual_imposto / 100
-                        ).toLocaleString('pt-BR', {
-                          style: 'percent',
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
-                        })}
-                      </span>
+                      <input
+                        type='text'
+                        name='nome'
+                        value={product.nome}
+                        onChange={onEditProduct}
+                      />
+                    ) : (
+                      <span>{nome}</span>
+                    )}
+                  </th>
+                  <th>
+                    {edit[index] ? (
+                      <input
+                        type='number'
+                        name='valor'
+                        value={parseFloat(product.valor)}
+                        onChange={onEditProduct}
+                      />
                     ) : (
                       <span>
-                        {(percentual_imposto / 100).toLocaleString('pt-BR', {
-                          style: 'percent',
-                          minimumFractionDigits: 2,
-                          maximumFractionDigits: 2,
+                        {valor.toLocaleString('pt-BR', {
+                          style: 'currency',
+                          currency: 'BRL',
                         })}
                       </span>
                     )}
+                  </th>
+                  <th>
                     {edit[index] ? (
-                      <CheckFat
-                        size={20}
-                        className='check'
-                        onClick={() => acceptEdit(index)}
-                      />
+                      <select name='tipo' onChange={onEditProduct} value={product.tipo}>
+                        {types.map((type) => (
+                          <option value={type.codigo} key={uuidv4()}>
+                            {type.nome}
+                          </option>
+                        ))}
+                      </select>
                     ) : (
-                      <Pencil
-                        size={20}
-                        className='pencil'
-                        onClick={() => editProduct(index)}
-                      />
+                      <span>{categoria}</span>
                     )}
-                    <Trash
-                      size={20}
-                      className='trash'
-                      onClick={() => removeProduct(codigo)}
-                    />
-                  </div>
-                </th>
-              </tr>
-            )
+                  </th>
+                  <th>
+                    <div className='row-total'>
+                      {edit[index] ? (
+                        <span>
+                          {(
+                            types.find((type) => type.codigo === product.tipo)
+                              ?.percentual_imposto / 100
+                          ).toLocaleString('pt-BR', {
+                            style: 'percent',
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })}
+                        </span>
+                      ) : (
+                        <span>
+                          {(percentual_imposto / 100).toLocaleString('pt-BR', {
+                            style: 'percent',
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })}
+                        </span>
+                      )}
+                      {edit[index] ? (
+                        <CheckFat
+                          size={20}
+                          className='check'
+                          onClick={() => acceptEdit(index)}
+                        />
+                      ) : (
+                        <Pencil
+                          size={20}
+                          className='pencil'
+                          onClick={() => editProduct(index)}
+                        />
+                      )}
+                      <Trash
+                        size={20}
+                        className='trash'
+                        onClick={() => removeProduct(codigo)}
+                      />
+                    </div>
+                  </th>
+                </tr>
+              )
+            )}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colSpan={4}>
+              <hr />
+            </th>
+          </tr>
+          {enabled ? (
+            <tr>
+              <th>
+                <input
+                  type='text'
+                  name='nome'
+                  placeholder='Produto'
+                  value={newProduct.nome}
+                  onChange={({ target: { name, value } }) =>
+                    setNewProduct({ ...newProduct, [name]: value })
+                  }
+                />
+              </th>
+              <th>
+                <input
+                  type='number'
+                  name='valor'
+                  value={newProduct.valor}
+                  onChange={({ target: { name, value } }) =>
+                    setNewProduct({ ...newProduct, [name]: value })
+                  }
+                />
+              </th>
+              <th>
+                <select
+                  name='tipo'
+                  value={newProduct.tipo}
+                  onChange={({ target: { name, value } }) =>
+                    setNewProduct({ ...newProduct, [name]: value })
+                  }
+                >
+                  {types.map((type) => (
+                    <option value={type.codigo} key={uuidv4()}>
+                      {type.nome}
+                    </option>
+                  ))}
+                </select>
+              </th>
+            </tr>
+          ) : (
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
           )}
-      </tbody>
-      <tfoot>
-        <tr>
-          <th colSpan={4}>
-            <hr />
-          </th>
-        </tr>
-      </tfoot>
-    </table>
+        </tfoot>
+      </table>
+      {enabled ? (
+        <div id='produto-buttons'>
+          <div className='add-produto' onClick={() => insertNewProduct()}>
+            <CheckCircle size={20} />
+            <span>Confirmar</span>
+          </div>
+          <div className='add-produto close' onClick={() => setEnabled(false)}>
+            <XCircle size={20} />
+            <span>Cancelar</span>
+          </div>
+        </div>
+      ) : (
+        <div className='add-produto' onClick={() => setEnabled(true)}>
+          <PlusCircle size={20} />
+          <span>Novo Produto</span>
+        </div>
+      )}
+    </div>
   );
 }
 
