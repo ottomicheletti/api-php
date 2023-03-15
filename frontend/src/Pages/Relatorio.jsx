e164f9599bf1f226f3950ea171911bd351f1d002
Otto Micheletti <michelettiotto@gmail.com>
2023-03-15 12:14:20 -0300

    feat: :sparkles: Criação da lógica para formatar os dados do relatório conforme o tipo de relatório escolhido.

--- frontend/src/Pages/Relatorio.jsx
+++ frontend/src/Pages/Relatorio.jsx
@@ -2,49 +2,83 @@
 import React, { useEffect } from 'react';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
+import { v4 as uuidv4 } from 'uuid';
 import Layout from '../Components/Layout';
 import { reportStore } from './../Store/Report';
 import request from '../Helpers/request';
 import './Relatorio.css';
 
 function Relatorio() {
-  //TODO  Melhorar formatação do PDF.
-
   const { report, orders, setReport, setOrders } = reportStore((state) => state);
 
-  const generateReport = (data, filename) => {
+  const generateReport = (data, filename, code = '') => {
+    //TODO  Melhorar formatação do PDF.
     const doc = new jsPDF();
     const head = Object.keys(data[0]).map((title) =>
       (title.charAt(0).toUpperCase() + title.slice(1)).replace(/[^A-Z0-9]+/gi, ' ')
     );
     const body = data.map((d) => Object.values(d));
 
+    switch (filename) {
+      case 'pedidos':
+        head[2] = 'Código';
+        body.forEach((arr) => {
+          arr[0] = ('00' + arr[0]).slice(-2); // CODIGO PEDIDO
+          arr[2] = ('00' + arr[2]).slice(-2); // CODIGO PRODUTO
+          arr[3] = arr[3].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // VALOR PRODUTO
+          arr[5] = arr[5].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // TOTAL PRODUTO
+          arr[6] = arr[6].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // IMPOSTO PRODUTO
+          const [year, month, day] = arr[7].slice(0, -9).split('-');
+          arr[7] = new Date(year, month - 1, day).toLocaleDateString(); // DATA
+        });
+        break;
+      case 'produtos':
+        head[0] = 'Código';
+        head[3] = 'Alíquota de Imposto';
+        body.forEach((arr) => {
+          arr[0] = ('00' + arr[0]).slice(-2); // CODIGO PRODUTO
+          arr[2] = arr[2].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); // VALOR PRODUTO
+          arr[3] = (arr[3] / 100).toLocaleString('pt-BR', { style: 'percent' }); // ALIQUOTA
+        });
+        break;
+      case 'tipos_produto':
+        head[0] = 'Código';
+        head[2] = 'Alíquota de Imposto';
+        body.forEach((arr) => {
+          arr[0] = ('00' + arr[0]).slice(-2); // CODIGO TIPO
+          arr[2] = (arr[2] / 100).toLocaleString('pt-BR', { style: 'percent' }); // ALIQUOTA
+        });
+        break;
+      default:
+        break;
+    }
+
     autoTable(doc, {
       head: [head],
       body,
     });
 
-    doc.save(`relatorio${filename}.pdf`);
+    doc.save(`relatorio_${filename}${code}.pdf`);
   };
 
   const handleGenerate = () => {
     switch (report.tipo) {
       case 'tipos':
         request('tipos_produto', 'GET').then((data) =>
-          generateReport(data, '_tipos_produto')
+          generateReport(data, 'tipos_produto')
         );
         break;
       case 'pedidos':
         if (report.qual !== 0) {
           request(`pedidos/${report.qual}`, 'GET').then((data) =>
-            generateReport(data, `_pedidos_${report.qual}`)
+            generateReport(data, 'pedidos', `_${report.qual}`)
           );
         } else {
-          request('pedidos', 'GET').then((data) => generateReport(data, '_pedidos'));
+          request('pedidos', 'GET').then((data) => generateReport(data, 'pedidos'));
         }
         break;
       default:
-        request('produtos', 'GET').then((data) => generateReport(data, '_produtos'));
+        request('produtos', 'GET').then((data) => generateReport(data, 'produtos'));
         break;
     }
   };
@@ -71,8 +105,8 @@ function Relatorio() {
               <select name='qual' onChange={setReport} value={report.qual}>
                 <option value='0'>Todos</option>
                 {orders.map((order) => (
-                  <option value={order.codigo} key={order.codigo}>
-                    {('00' + order.codigo).slice(-2)}
+                  <option value={order} key={uuidv4()}>
+                    {('00' + order).slice(-2)}
                   </option>
                 ))}
               </select>
